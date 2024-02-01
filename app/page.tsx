b26b6140365b7b38952223e7dd57da699a504c3a
Zhenbin Lin <zhenbin2004@gmail.com>
2024-01-31 23:55:24 -0500

    make login page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,47 @@
 "use client"
 import { Button } from './components/Button';
 import HomePage from './components/Homepage';
+import LoginPage from './components/LoginPage';
 import { auth } from './firebase';
 import { GoogleAuthProvider, signInWithPopup, } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function Home() {
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
-    hd: "stonybrook.edu"
-  })
-  const [user, loading] = useAuthState(auth);
+    const googleProvider = new GoogleAuthProvider();
+    googleProvider.setCustomParameters({
+        hd: "stonybrook.edu"
+    })
+    const [user, loading] = useAuthState(auth);
 
 
-  if (loading) {
+    if (loading) {
+        return (
+            <main className="flex justify-center p-24">
+                <div>
+                    <h1>Loading...</h1>
+                </div>
+            </main >
+        )
+    }
+    if (user) {
+        return (
+            <main className="flex min-h-screen flex-col items-center p-24">
+                <div>
+                    <HomePage user={user} auth={auth} />
+                </div>
+            </main >
+        )
+    }
+
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, googleProvider);
+        console.log(result.user.email);
+    }
+
     return (
-      <main className="flex justify-center p-24">
-        <div>
-          <h1>Loading...</h1>
-        </div>
-      </main >
+        <main className="">
+            <LoginPage onClickSignInWithGoogle={signInWithGoogle}/>
+        </main >
     )
-  }
-  if (user) {
-    return (
-      <main className="flex min-h-screen flex-col items-center p-24">
-        <div>
-          <HomePage user={user} auth={auth} />
-        </div>
-      </main >
-    )
-  }
-
-  const signIn = async () => {
-    const result = await signInWithPopup(auth, provider);
-    console.log(result.user.email);
-  }
-
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div>
-      </div>
-      LOQI
-      <Button size='sm' variant='outline' onClick={signIn}>
-        Continue With Google
-      </Button>
-      <div>
-      </div>
-    </main >
-  )
 }

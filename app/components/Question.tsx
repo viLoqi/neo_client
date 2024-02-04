65e2f651e999ca4302238e470292b292aa18f81d
Zhenbin Lin <zhenbin2004@gmail.com>
2024-02-04 13:35:52 -0500

    Made questions card more responsive

--- app/components/Question.tsx
+++ app/components/Question.tsx
@@ -26,21 +26,20 @@ const Question = ({ card, cards, setNextCard }: Props) => {
     const deckName = "DECK NAME"
 
     return (
-        <div className="flex w-full h-screen ">
-            <div className="flex flex-col items-center w-full bg-[#18593c]">
+        <div className="flex h-screen">
+            <div className="flex flex-col w-full h-full bg-[#18593c]">
                 {/* Course title */}
                 <h1 className="flex w-full justify-center text-white font-semibold border-b-[1px] py-2 mb-1 border-black">
                     {course}
                 </h1>
                 {/* main body */}
-                <div className="flex w-full ml-12">
+                <div className="flex h-full pl-12">
                     {/* Left side */}
-                    <div className="flex flex-col mr-8">
+                    <div className="flex flex-col">
                         {/* Back button */}
                         <Link href="/">
                             <CiCircleChevLeft size="50" color="white" />
                         </Link>
-                        {/* Dummy timer, will update soon */}
                         <div className="flex mt-[30vh] items-center justify-center text-white text-xl font-semibold size-36 justify-self-center">
                             <CountdownCircleTimer
                                 isPlaying
@@ -59,35 +58,37 @@ const Question = ({ card, cards, setNextCard }: Props) => {
                     </div>
 
                     {/* Card */}
-                    <div className="flex flex-col w-[1000px] py-8 my-8 items-center justify-evenly rounded-[50px] bg-white">
-                        {/* Deck title */}
-                        <h1 className="font-bold text-3xl">
-                            {deckName}
-                        </h1>
-                        {/* Question */}
-                        <div className="w-4/5 mt-5">
-                            <p className="whitespace-pre font-bold text-sm">
-                                {card.question}
-                            </p>
-                        </div>
-                        {/* Answer choices */}
-                        <div className="grid grid-cols-2 gap-3 w-4/5">
-                            {["WRONG", "WRONG", "WRONG", card.answer].map((choice) => {
-                                return (
-                                    <ChoiceButton
-                                        key={choice}
-                                        text={choice}
-                                        isCorrect={choice === card.answer}
-                                    />
-                                );
-                            })}
+                    <div className="flex w-full h-full mx-20 items-center">
+                        <div className="flex flex-col w-full h-4/5 py-8 items-center justify-evenly rounded-[50px] bg-white">
+                            {/* Deck title */}
+                            <h1 className="font-bold text-3xl">
+                                {deckName}
+                            </h1>
+                            {/* Question */}
+                            <div className="w-4/5 mt-5">
+                                <p className="whitespace-pre font-bold text-sm">
+                                    {card.question}
+                                </p>
+                            </div>
+                            {/* Answer choices */}
+                            <div className="grid grid-cols-2 gap-3 w-4/5">
+                                {["WRONG", "WRONG", "WRONG", card.answer].map((choice) => {
+                                    return (
+                                        <ChoiceButton
+                                            key={choice}
+                                            text={choice}
+                                            isCorrect={choice === card.answer}
+                                        />
+                                    );
+                                })}
 
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             {/* Users panel */}
-            <UsersPanel/>
+            <UsersPanel />
         </div>
     );
 };

95e8a1e3efbaff6a5af40e239b09757ef2e6addb
wilsonw13 <wwuchen1309@gmail.com>
2024-04-19 02:39:50 -0400

    updated the pricing page to match the figma

--- app/(routes)/pricing/page.tsx
+++ app/(routes)/pricing/page.tsx
@@ -1,64 +1,178 @@
 "use client"
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/app/_modules/firebase"
 
 import { Navbar } from "@/app/_components/Navbar";
 
+interface tierCard {
+    tier: string;
+    text: string;
+    cost: number;
+    requests: number;
+    grad_bg_color: string; // gradiant color (starts at the top)
+    accent_bg_color: string; // the bg-color of the top bar and (optionally) the get started button
+    isMostPopular: boolean;
+}
+
+interface testimonialCard {
+    image_url: string;
+    text: string;
+    text_bubble_color: string;
+    isFacingRight?: boolean;
+}
+
 const PricingPage = () => {
     const [user, loading] = useAuthState(auth);
-    const [tier, setTier] = useState("Free");
+    const [userTier, setUserTier] = useState("");
 
     useEffect(() => {
         if (!loading)
             fetch(`/api/payment/user?email=${user?.email}`).then((res) => res.json())
-                .then((userData) => setTier(userData.tier)).catch((e) => console.log("User not found!"));
+                .then((userData) => setUserTier(userData.tier)).catch((e) => console.log("User not found!"));
     }, [loading, user?.email])
 
 
-    // =========== STYLING ===========
-    const flexChildStyle = "m-7 py-6 flex flex-col justify-evenly items-center gap-y-2";
-    const shoppingCartSVG = <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" id="cart"><path d="M14 36c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79 4-4-1.79-4-4-4zM2 4v4h4l7.19 15.17-2.7 4.9c-.31.58-.49 1.23-.49 1.93 0 2.21 1.79 4 4 4h24v-4H14.85c-.28 0-.5-.22-.5-.5 0-.09.02-.17.06-.24L16.2 26h14.9c1.5 0 2.81-.83 3.5-2.06l7.15-12.98c.16-.28.25-.61.25-.96a2 2 0 0 0-2-2H10.43l-1.9-4H2zm32 32c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79 4-4-1.79-4-4-4z"></path><path fill="none" d="M0 0h48v48H0z"></path></svg>
+    const tierCard = ({ tier, text, cost, requests, grad_bg_color, accent_bg_color, isMostPopular }: tierCard) => {
+        const isCurrentTier: boolean = tier === userTier;
+        const displayTopBar: boolean = isCurrentTier || isMostPopular;
 
-    return (
-        <div>
-            <Navbar />
-            <div className="text-center">
-                <h1 className="text-3xl m-5">Loqi Subscription</h1>
-                <p className="text-gray-500 m-4">Logged in as {user?.displayName}</p>
-                <p className="text-[#FFD700] m-4 font-bold">Current Tier: {tier}</p>
-
-                <div className="w-full flex justify-evenly">
-                    <div className={"w-1/4 h-screen " + (tier === "Free" ? "bg-[#FFD700]/[.50]" : "")}>
-                        <div className={flexChildStyle + " bg-red-300"}>
-                            <p className="text-2xl">Free</p>
-                            <p className="text-1xl">$0 / month</p>
-                        </div>
-                    </div>
-
-                    <div className={"w-1/4 h-screen " + (tier === "Basic" ? "bg-[#FFD700]/[.50]" : "")}>
-                        <Link className={flexChildStyle + " bg-lime-300 cursor-pointer"} href="/checkout/basic">
-                            <p className="text-2xl">Basic</p>
-                            <p className="text-1xl">$5 / month</p>
-                            {shoppingCartSVG}
-                        </Link>
-                    </div>
-
-                    <div className={"w-1/4 h-screen " + (tier === "Premium" ? "bg-[#FFD700]/[.50]" : "")}>
-                        <Link className={flexChildStyle + " bg-green-500 cursor-pointer"} href="/checkout/premium">
-                            <p className="text-2xl">Premium</p>
-                            <p className="text-1xl">$10 / month</p>
-                            {shoppingCartSVG}
-                        </Link>
-                    </div>
+        return (
+            <div className="text-left max-w-[17rem]">
+                {/* (Optional) Most Popular / Your Current Tier */}
+                <div className={`p-1 rounded-t-3xl border-app-black border-[1px] ${displayTopBar ? '' : 'opacity-0'} ${accent_bg_color}`}>
+                    <h1 className="text-xl font-bold text-center">{isCurrentTier ? "YOUR CURRENT TIER" : "Most Popular"}</h1>
                 </div>
 
+                <div className={`py-6 px-8 ${displayTopBar ? "rounded-b-3xl" : "rounded-3xl"} flex flex-col gap-y-7 bg-gradient-to-b ${grad_bg_color} to-app-white`}>
+                    <h1 className="block text-4xl font-bold">{tier}</h1>
+                    <h2 className="">{text}</h2>
+
+                    <div className="flex justify-between items-center font-bold">
+                        <h1 className="text-4xl">${cost}</h1>
+                        <p className="text-sm text-right">per month</p>
+                    </div>
+
+                    <div className="flex justify-between items-center font-bold">
+                        <h1 className="text-4xl">{requests}</h1>
+                        <p className="text-sm text-right">requests<br />per month</p>
+                    </div>
+
+                    <Link className={`py-1 px-5 place-self-center rounded-2xl border-app-black border-[1px] font-bold bg-app-white`} href={"#"}>Get Started</Link>
+                </div>
             </div>
-        </div>)
+        );
+    }
+
+    const tiers: tierCard[] = [
+        {
+            tier: "Free",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            cost: 0,
+            requests: 300,
+            grad_bg_color: "from-purple-300",
+            accent_bg_color: "bg-purple-400",
+            isMostPopular: false,
+        },
+        {
+            tier: "Basic",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            cost: 5,
+            requests: 1000,
+            grad_bg_color: "from-yellow-200",
+            accent_bg_color: "bg-yellow-300",
+            isMostPopular: true,
+        },
+        {
+            tier: "Premium",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            cost: 10,
+            requests: 5000,
+            grad_bg_color: "from-green-300",
+            accent_bg_color: "bg-green-400",
+            isMostPopular: false,
+        },
+    ]
+
+    const testimonialCard = ({ image_url, text, text_bubble_color, isFacingRight = true }: testimonialCard) => {
+        return (
+            <div className={`flex ${isFacingRight ? "flex-row-reverse" : "flex-row"} justify-start gap-6`}>
+                {/* Profile Picture */}
+                <div className="w-[8rem] h-[8rem] relative flex-none bg-red-300 rounded-full border-app-accent border-4">
+                    <Image
+                        src={image_url}
+                        alt="User Testimonial Picture"
+                        layout="fill"
+                        objectFit="cover"
+                        className="rounded-full absolute"
+                    />
+                </div>
+
+                {/* Speech Bubble */}
+                <div className={`h-[8rem] flex ${isFacingRight ? "flex-row-reverse justify-start" : "flex-row justify-end"} items-center`}>
+                    <div className="w-3 overflow-hidden">
+                        <div className={`h-4 ${text_bubble_color} rotate-45 transform ${isFacingRight ? "origin-top-left" : "origin-bottom-right"} rounded-sm`}></div>
+                    </div>
+                    <div className={`h-[8rem] max-h-[8rem] ${text_bubble_color} p-4 rounded-lg flex-1 ${isFacingRight ? "justify-end text-right" : "justify-start text-left"} flex items-center`}>{text}</div>
+                </div>
+            </div>
+        );
+    }
+
+    const testimonials: testimonialCard[] = [
+        {
+            image_url: "/dummy-user-pic.jpg",
+            text: "Crazy? I was crazy once! I was stuck in a room. A rubber room. A rubber room filled with rats. And rats make me crazy. Crazy? I was crazy once! I was stuck in a room. A rubber room. A rubber room filled with rats. And rats make me crazy.",
+            text_bubble_color: "bg-green-400"
+        },
+        {
+            image_url: "/google-logo.png",
+            text: "Loqi is legit the greatest app to have existed of all time!",
+            text_bubble_color: "bg-green-400"
+        },
+        {
+            image_url: "/google-logo.png",
+            text: "Loqi is legit the greatest app to have existed of all time!",
+            text_bubble_color: "bg-green-400"
+        },
+        {
+            image_url: "/dummy-user-pic.jpg",
+            text: "Crazy? I was crazy once! I was stuck in a room. A rubber room. A rubber room filled with rats. And rats make me crazy. Crazy? I was crazy once! I was stuck in a room. A rubber room. A rubber room filled with rats. And rats make me crazy.",
+            text_bubble_color: "bg-green-400"
+        },
+    ]
 
 
+    return (
+        <div className="min-h-screen pb-[4rem] bg-app-primary">
+            <Navbar />
+            <div className="text-center mt-8">
+                <div className="flex flex-col place-items-center gap-7">
+                    <h1 className="text-5xl font-bold text-app-white">Pricing Plans</h1>
+                    <p className="text-xl font-bold text-app-white">Choose the plan that best suits your needs</p>
+
+                    {/* Displays all current tiers */}
+                    <div className="w-[80%] py-8 px-10 bg-app-gray rounded-[4rem] flex gap-10 justify-center">
+                        {tiers.map(t => tierCard(t))}
+                    </div>
+
+                    <h1 className="mt-2 text-5xl font-bold text-app-white">Testimonials</h1>
+                    <p className="text-xl font-bold text-app-white">Hear from users who have succeeded</p>
+
+                    {/* Displays all testimonials */}
+                    <div className="w-[80%] py-10 px-[5rem] bg-app-gray rounded-[4rem] flex flex-col gap-10 justify-center">
+                        {testimonials.map((t, i) => [
+                            testimonialCard({ ...t, isFacingRight: !!(i % 2) }),
+                            i < testimonials.length - 1 && <div className="w-100 h-[0.125rem] bg-app-accent" /> // this i the divider
+                        ])}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default PricingPage;
